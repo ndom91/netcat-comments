05fa229f6b07d02411e2bb074ad984856b696cae
ndom91 <yo@ndo.dev>
2024-02-13 17:45:07 +0100

    fix: pipe data back via async stream pipeline and async generator

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,37 @@
-import net, { type Socket } from "node:net";
-import { Buffer } from "node:buffer";
-import { buildRequestObject, validateBody } from "./utils";
+import net from "node:net";
 import { ValiError } from "valibot";
+import { Buffer } from "node:buffer";
+import { pipeline } from "node:stream/promises"
+import { buildRequestObject, validateBody } from "./utils";
 
-const server = net.createServer((socket: Socket) => {
-  socket
-    .on('data', (chunk: Buffer | string) => {
-      try {
-        const messages = Buffer.from(chunk).toString().split('\n').filter(Boolean)
-        console.log('\n\nbodyString', messages)
-        const parsedMessages = messages.map(msg => {
-          const rawBodyObject = buildRequestObject(msg)
-          console.log('\n\nrawBodyObject', rawBodyObject)
-          const body = validateBody(rawBodyObject)
-          console.log('\n\nbody', body)
-          return body
-        })
+const server = net.createServer((socket) => {
+  pipeline(
+    socket,
+    async function*(source) {
+      for await (const chunk of source) {
+        try {
+          const messages = Buffer.from(chunk).toString().split('\n').filter(Boolean)
+          const parsedMessages = messages.map(msg => {
+            const rawBodyObject = buildRequestObject(msg)
+            const body = validateBody(rawBodyObject)
+            return body
+          })
 
-        console.log('parsedMessages', parsedMessages)
-      } catch (e) {
-        if (e instanceof ValiError) {
-          console.log(e)
-          const issuesResponse = e.issues.map(issue => `Input "${issue.input}" invalid: ${issue.message}`).join('\n')
-          console.log(issuesResponse)
+          let response = parsedMessages[0].requestId
+          if (parsedMessages[0].data) {
+            response += `|${parsedMessages[0].data}`
+          }
+
+          yield `${response}\n`
+        } catch (e) {
+          if (e instanceof ValiError) {
+            yield e.issues.map(issue => `Input "${issue.input}" invalid: ${issue.message}`).join('\n') + '\n'
+          }
         }
       }
-    })
-  socket.pipe(socket);
+    },
+    socket
+  )
 });
 
 server.listen(1337, "127.0.0.1");
