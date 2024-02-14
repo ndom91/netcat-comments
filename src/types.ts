f9a5f0edd45077354f157d92d697f0a45af06d05
ndom91 <yo@ndo.dev>
2024-02-14 14:19:47 +0100

    fix: add runtime conditional type verification for data key

--- src/types.ts
+++ src/types.ts
@@ -6,23 +6,57 @@ export const Actions = {
   SIGN_OUT: "SIGN_OUT",
   CREATE_DISCUSSION: "CREATE_DISCUSSION",
   CREATE_REPLY: "CREATE_REPLY",
+  GET_DISCUSSION: "GET_DISCUSSION",
 } as const;
 
-export const requestBodySchema = v.object({
+export const Table = {
+  AUTH: "AUTH",
+  DISCUSSION: "DISCUSSION",
+  COMMENT: "COMMENT"
+} as const
+
+const ActionSchemas = {
+  [Actions.SIGN_IN]: v.array(v.string([v.minLength(1)]), [v.minLength(1)]),
+  [Actions.SIGN_OUT]: v.array(v.string([v.minLength(1)]), [v.minLength(1)]),
+  [Actions.CREATE_DISCUSSION]: v.array(v.string([v.minLength(1)]), [v.minLength(1)]),
+  [Actions.GET_DISCUSSION]: v.array(v.string([v.minLength(1)]), [v.minLength(1)]),
+  [Actions.CREATE_REPLY]: v.array(v.string([v.minLength(1)]), [v.minLength(1)]),
+  [Actions.WHOAMI]: v.array(v.null_('No data'), [v.maxLength(0)])
+}
+
+export const RequestBodySchema = v.object({
   requestId: v.string([v.length(7), v.regex(/[a-zA-Z]/)]),
-  action: v.nullable(v.optional(v.enum_(Actions))),
-  data: v.nullable(v.optional(v.string())),
-});
+  action: v.enum_(Actions),
+  data: v.nullable(v.optional(v.array(v.string()))),
+  type: v.enum_(Table)
+}, [
+  v.forward(
+    v.custom(({ action, data }) => {
+      const parseResult = v.safeParse(ActionSchemas[action], data)
+      if (parseResult.success) {
+        return true
+      }
+      return false
+    },
+      "Bad data"),
+    ['data'])
+])
 
-export type MessageBody = v.Output<typeof requestBodySchema>
-export type Message = { key: string, data: MessageBody }
+export const RequestBodySchema2 = v.object({
+  requestId: v.string([v.length(7), v.regex(/[a-zA-Z]/)]),
+  action: v.enum_(Actions),
+  data: v.nullable(v.optional(v.array(v.string()))),
+  type: v.enum_(Table)
+})
 
-export type ParsedAuthBody = {
+export type MessageBody = v.Output<typeof RequestBodySchema>
+export type Message = {
   key: string,
   data: MessageBody
 }
 
-export const Table = {
-  AUTH: "AUTH",
-  DISCUSSION: "DISCUSSION"
-} as const
+export type ParsedBody = {
+  key: string,
+  type: keyof typeof Table,
+  data: MessageBody
+}
